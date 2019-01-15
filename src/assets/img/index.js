61c159b0802e0af7a612273904ae8589687bc4bd
Pol Layola <pol.layola@ext.privalia.com>
2019-01-15 09:26:40 +0100

    ESLint implementation

--- src/assets/img/index.js
+++
@@ -1,4 +0,0 @@
-export const image = {
-  reactIcon: require('./react-icon.png'),
-  reduxFormIcon: require('./redux-form-icon.png'),
-};
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { image } from '@Assets/img';
-import {
-  HeaderWrapper,
-  Image,
-  Title,
-} from './styles';
+import styled from 'styled-components';
+import reduxFormIcon from '@Assets/img/redux-form-icon.png';
 
-const Header = () => (
+const HeaderWrapper = styled.div`
+  height: 150px;
+  padding-top: 20px;
+  text-align: center;
+  box-shadow: 0 0 10px rgba(0,0,0,0.3);
+`;
+
+const Image = styled.img`
+  width: 100px;
+`;
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.primary};
+`;
+
+export const Header = () => (
   <HeaderWrapper>
-    <Image src={image.reduxFormIcon} />
+    <Image src={reduxFormIcon} />
     <Title>Form Validation UI</Title>
   </HeaderWrapper>
 );
-
-export default Header;
